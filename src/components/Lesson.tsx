57c8ba2a3345953ef2cf5b7ef0c42c401c88b287
André Ferreira <andre.ferreira.alves@outlook.com>
2022-06-27 22:01:49 -0300

    aula 3

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react';
 import { isPast, format } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { Link } from 'react-router-dom';
 
 interface LessonType {
     title: string;
@@ -16,12 +17,12 @@ export function Lesson(props: LessonType) {
     })
 
     return (
-        <a href="#">
+        <Link to={`/event/lesson/${props.slug}`} className="group">
             <span className="text-gray-300">
                 {avaliableDateFormat}
             </span>
 
-            <div className="rounded border border-gray-500 p-4 mt-2">
+            <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
                 <header className="flex items-center justify-between">
                     {
 
@@ -48,6 +49,6 @@ export function Lesson(props: LessonType) {
                     {props.title}
                 </strong>
             </div>
-        </a>
+        </Link>
     );
 }
