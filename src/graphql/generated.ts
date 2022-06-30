f5269bf6fe284fec5a767c8370dbf9156e34de56
André Ferreira <andre.ferreira.alves@outlook.com>
2022-06-30 20:39:45 -0300

    create subscriber

--- src/graphql/generated.ts
+++ src/graphql/generated.ts
@@ -5568,6 +5568,14 @@ export enum _SystemDateTimeFieldVariation {
   Localization = 'localization'
 }
 
+export type CreateSubscriberMutationVariables = Exact<{
+  name: Scalars['String'];
+  email: Scalars['String'];
+}>;
+
+
+export type CreateSubscriberMutation = { __typename?: 'Mutation', createSubscriber?: { __typename?: 'Subscriber', id: string } | null };
+
 export type GetLessonBySlugQueryVariables = Exact<{
   slug?: InputMaybe<Scalars['String']>;
 }>;
@@ -5581,6 +5589,40 @@ export type GetLessonsQueryQueryVariables = Exact<{ [key: string]: never; }>;
 export type GetLessonsQueryQuery = { __typename?: 'Query', lessons: Array<{ __typename?: 'Lesson', id: string, lessonType: LessonType, availableAt?: any | null, title: string, slug?: string | null }> };
 
 
+export const CreateSubscriberDocument = gql`
+    mutation createSubscriber($name: String!, $email: String!) {
+  createSubscriber(data: {name: $name, email: $email}) {
+    id
+  }
+}
+    `;
+export type CreateSubscriberMutationFn = Apollo.MutationFunction<CreateSubscriberMutation, CreateSubscriberMutationVariables>;
+
+/**
+ * __useCreateSubscriberMutation__
+ *
+ * To run a mutation, you first call `useCreateSubscriberMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateSubscriberMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createSubscriberMutation, { data, loading, error }] = useCreateSubscriberMutation({
+ *   variables: {
+ *      name: // value for 'name'
+ *      email: // value for 'email'
+ *   },
+ * });
+ */
+export function useCreateSubscriberMutation(baseOptions?: Apollo.MutationHookOptions<CreateSubscriberMutation, CreateSubscriberMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateSubscriberMutation, CreateSubscriberMutationVariables>(CreateSubscriberDocument, options);
+      }
+export type CreateSubscriberMutationHookResult = ReturnType<typeof useCreateSubscriberMutation>;
+export type CreateSubscriberMutationResult = Apollo.MutationResult<CreateSubscriberMutation>;
+export type CreateSubscriberMutationOptions = Apollo.BaseMutationOptions<CreateSubscriberMutation, CreateSubscriberMutationVariables>;
 export const GetLessonBySlugDocument = gql`
     query GetLessonBySlug($slug: String) {
   lesson(where: {slug: $slug}) {
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,6 +1,30 @@
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
+import { useCreateSubscriberMutation } from "../graphql/generated";
 
 export function Subscribe() {
+    const navigate = useNavigate();
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+
+    const [createSubscriber, { data, loading }] = useCreateSubscriberMutation()
+    console.log(data);
+
+    async function handleSubscribe(form: FormEvent) {
+        form.preventDefault()
+
+        await createSubscriber({
+            variables: {
+                name,
+                email
+            }
+        })
+
+        navigate('/event')
+    }
+
     return (
         <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
             <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
@@ -16,16 +40,23 @@ export function Subscribe() {
                 <div className="p-8 bg-gray-700 border border-gray-500 rounded">
                     <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
 
-                    <form onSubmit={() => { }} className="flex flex-col w-full gap-2">
+                    <form onSubmit={handleSubscribe} className="flex flex-col w-full gap-2">
                         <input className="bg-gray-900 rounded px-5 h-14"
                             type="text"
-                            placeholder="Seu nome completo" />
+                            placeholder="Seu nome completo"
+                            onChange={(event) => setName(event.target.value)}
+                        />
 
                         <input className="bg-gray-900 rounded px-5 h-14"
                             type="email"
-                            placeholder="Digite seu email" />
+                            placeholder="Digite seu email"
+                            onChange={(event) => setEmail(event.target.value)}
+                        />
 
-                        <button type="submit" className="mt-4 bg-green-500 uppercase py-4 rounded text-sm hover:bg-green-700 transition-colors disabled:opacity-50">
+                        <button
+                            disabled={loading}
+                            type="submit"
+                            className="mt-4 bg-green-500 uppercase py-4 rounded text-sm hover:bg-green-700 transition-colors disabled:opacity-50">
                             Garantir minha vaga
                         </button>
                     </form>
